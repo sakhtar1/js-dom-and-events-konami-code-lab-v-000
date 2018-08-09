a89543a801805f92f39605d61d23255d840ef328
Sharmina Akhtar <sakht318@gmail.com>
2018-08-09 09:22:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
+
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
+  let index = 0
+  const keydown = parseInt(e.detail || e.which);
 
-    if (key === code[index]) {
+    if (keydown === code[index]) {
       index++;
 
       if (index === code.length) {
