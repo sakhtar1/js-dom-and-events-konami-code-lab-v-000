e819d3925e77bc3f95d32c6b2cf529e4eb484da0
Sharmina Akhtar <sakht318@gmail.com>
2018-08-09 09:26:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0
   document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which || e.location);
+  const key = parseInt(e.detail || e.location);
 
     if (key === code[index]) {
       index++;
